3946732614d5a5786a95736260fdb309bc4e10cd
tandica <williams.tandica@gmail.com>
2021-05-24 21:34:46 +0000

    added more queries

--- 1_queries/total_students_in_cohorts.sql
+++ 1_queries/total_students_in_cohorts.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(*)
 FROM students
-WHErE cohort_id IN (1, 2, 3)
+WHERE cohort_id IN (1, 2, 3)
---
+++ 3_queries_group-by/low_avg_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT student.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration;

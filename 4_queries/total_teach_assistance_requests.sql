6c6e6fa8b200364e41b1047375757fa5fefe378d
tandica <williams.tandica@gmail.com>
2021-05-25 18:29:55 +0000

    added queries

---
+++ 4_queries/total_teach_assistance_requests.sql
@@ -0,0 +1,5 @@
+SELECT tearchers.name, COUNT(assistance_requests.*) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;

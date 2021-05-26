4b1ac17cfd34cf29fa15040e0fafe1dace4c28ef
tandica <williams.tandica@gmail.com>
2021-05-26 19:43:27 +0000

    updated js code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,37 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+let cohortName = [`${process.argv[2]}`];
+
+pool.query(queryString, cohortName)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+
+
+//vulnerable to sql injection attacks
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '%${process.argv[2] || 'JUL02'}%'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
